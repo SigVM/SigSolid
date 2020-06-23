b2dfda846f31cd2a26482a6a2dd274049c1e5423
zandent <simon.zhao@mail.utoronto.ca>
2020-06-22 20:41:45 -0400

    add tb

---
+++ signalslot_parse_script/tb/tb1/testContract.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.9;
+contract newtestContract {
+	signal PriceFeedUpdate(uint data);uint x;uint y;
+	constructor() public {
+		{PriceFeedUpdate();}
+	}
+}
+contract testContract {
+	newtestContract dut;
+	uint price_xyz;
+	uint public constant ONE_HOUR = 180; // 3600/20
+	slot what_ever(uint obj){
+		{{}{}{}{price_xyz = obj;}}
+		{{}{}{}{price_xyz = obj;}}
+		{{}{}{}{price_xyz = obj;}}
+		{{}{}{}{price_xyz = obj;}}{
+	}}
+	function bindfunc() public view{
+		what_ever.bind(dut.PriceFeedUpdate);
+	}
+    function emitfunc() public view{
+		emitsig dut.PriceFeedUpdate(price_xyz).delay(ONE_HOUR);
+    }
+}
+
+
+
