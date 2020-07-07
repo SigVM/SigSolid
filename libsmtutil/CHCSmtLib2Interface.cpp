b20f4761f0e6006b0e69b827832e0003b86dfb89
Leonardo Alt <leo@ethereum.org>
2020-07-07 16:22:51 +0200

    Convert z3 cex graph into STL

--- libsmtutil/CHCSmtLib2Interface.cpp
+++ libsmtutil/CHCSmtLib2Interface.cpp
@@ -82,7 +82,7 @@ void CHCSmtLib2Interface::addRule(Expression const& _expr, std::string const& _n
 	);
 }
 
-pair<CheckResult, vector<string>> CHCSmtLib2Interface::query(Expression const& _block)
+pair<CheckResult, CHCSolverInterface::CexGraph> CHCSmtLib2Interface::query(Expression const& _block)
 {
 	string accumulated{};
 	swap(m_accumulatedOutput, accumulated);
@@ -107,7 +107,7 @@ pair<CheckResult, vector<string>> CHCSmtLib2Interface::query(Expression const& _
 		result = CheckResult::ERROR;
 
 	// TODO collect invariants or counterexamples.
-	return make_pair(result, vector<string>{});
+	return {result, {}};
 }
 
 void CHCSmtLib2Interface::declareVariable(string const& _name, SortPointer const& _sort)
