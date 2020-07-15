ethereum/solidity

[yul] Adding support for accessing subobjects via

libyul/backends/evm/AbstractAssembly.h
@@ -100,9 +100,9 @@ public:
 	/// Creates a new sub-assembly, which can be referenced using dataSize and dataOffset.
 	virtual std::pair<std::shared_ptr<AbstractAssembly>, SubID> createSubAssembly() = 0;
 	/// Appends the offset of the given sub-assembly or data.
-	virtual void appendDataOffset(SubID _sub) = 0;
+	virtual void appendDataOffset(std::vector<SubID> const& _subIds) = 0;
 	/// Appends the size of the given sub-assembly or data.
-	virtual void appendDataSize(SubID _sub) = 0;
+	virtual void appendDataSize(std::vector<SubID> const& _subIds) = 0;
 	/// Appends the given data to the assembly and returns its ID.
 	virtual SubID appendData(bytes const& _data) = 0;
 

libyul/backends/evm/AsmCodeGen.h
@@ -57,8 +57,8 @@ public:
 	void appendReturnsub(int, int) override;
 	void appendAssemblySize() override;
 	std::pair<std::shared_ptr<AbstractAssembly>, SubID> createSubAssembly() override;
-	void appendDataOffset(SubID _sub) override;
-	void appendDataSize(SubID _sub) override;
+	void appendDataOffset(std::vector<SubID> const& _subIds) override;
+	void appendDataSize(std::vector<SubID> const& _subIds) override;
 	SubID appendData(bytes const& _data) override;
 
 	void appendImmutable(std::string const& _identifier) override;

libyul/backends/evm/NoOutputAssembly.h
@@ -67,8 +67,8 @@ public:
 
 	void appendAssemblySize() override;
 	std::pair<std::shared_ptr<AbstractAssembly>, SubID> createSubAssembly() override;
-	void appendDataOffset(SubID _sub) override;
-	void appendDataSize(SubID _sub) override;
+	void appendDataOffset(std::vector<SubID> const& _subIds) override;
+	void appendDataSize(std::vector<SubID> const& _subIds) override;
 	SubID appendData(bytes const& _data) override;
 
 	void appendImmutable(std::string const& _identifier) override;

SigVM/SigSolid

libyul/backends/evm/AbstractAssembly.h
@@ -101,9 +101,9 @@ public:
 	/// Creates a new sub-assembly, which can be referenced using dataSize and dataOffset.
 	virtual std::pair<std::shared_ptr<AbstractAssembly>, SubID> createSubAssembly() = 0;
 	/// Appends the offset of the given sub-assembly or data.
-	virtual void appendDataOffset(SubID _sub) = 0;
+	virtual void appendDataOffset(std::vector<SubID> const& _subIdPath) = 0;
 	/// Appends the size of the given sub-assembly or data.
-	virtual void appendDataSize(SubID _sub) = 0;
+	virtual void appendDataSize(std::vector<SubID> const& _subIdPath) = 0;
 	/// Appends the given data to the assembly and returns its ID.
 	virtual SubID appendData(bytes const& _data) = 0;
 

libyul/backends/evm/AsmCodeGen.h
@@ -57,8 +57,8 @@ public:
 	void appendReturnsub(int, int) override;
 	void appendAssemblySize() override;
 	std::pair<std::shared_ptr<AbstractAssembly>, SubID> createSubAssembly() override;
-	void appendDataOffset(SubID _sub) override;
-	void appendDataSize(SubID _sub) override;
+	void appendDataOffset(std::vector<SubID> const& _subIdPath) override;
+	void appendDataSize(std::vector<SubID> const& _subIdPath) override;
 	SubID appendData(bytes const& _data) override;
 
 	void appendImmutable(std::string const& _identifier) override;

libyul/backends/evm/NoOutputAssembly.h
@@ -67,8 +67,8 @@ public:
 
 	void appendAssemblySize() override;
 	std::pair<std::shared_ptr<AbstractAssembly>, SubID> createSubAssembly() override;
-	void appendDataOffset(SubID _sub) override;
-	void appendDataSize(SubID _sub) override;
+	void appendDataOffset(std::vector<SubID> const& _subIdPath) override;
+	void appendDataSize(std::vector<SubID> const& _subIdPath) override;
 	SubID appendData(bytes const& _data) override;
 
 	void appendImmutable(std::string const& _identifier) override;