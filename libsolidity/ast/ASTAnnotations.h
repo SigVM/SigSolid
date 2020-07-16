asbish/solidity

NatSpec: Implement ``@inheritdoc``

libsolidity/ast/ASTAnnotations.h
@@ -78,6 +78,8 @@ struct StructurallyDocumentedAnnotation
 
 	/// Mapping docstring tag name -> content.
 	std::multimap<std::string, DocTag> docTags;
+	/// contract that @inheritdoc references if it exists
+	ContractDefinition const* inheritContract = nullptr;
 };
 
 struct SourceUnitAnnotation: ASTAnnotation

SigVM/SigSolid

libsolidity/ast/ASTAnnotations.h
@@ -78,6 +78,8 @@ struct StructurallyDocumentedAnnotation
 
 	/// Mapping docstring tag name -> content.
 	std::multimap<std::string, DocTag> docTags;
+	/// contract that @inheritdoc references if it exists
+	ContractDefinition const* inheritdocReference = nullptr;
 };
 
 struct SourceUnitAnnotation: ASTAnnotation