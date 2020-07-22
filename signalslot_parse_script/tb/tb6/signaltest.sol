8e32e0e7932f525537c00f6b5df80f21185e0794
R-Song <ryansong02@gmail.com>
2020-07-22 11:37:37 -0400

    Added comments to parsed code.

--- signalslot_parse_script/tb/tb6/signaltest.sol
+++ signalslot_parse_script/tb/tb6/signaltest.sol
@@ -1,22 +1,33 @@
 pragma solidity ^0.6.9;
+
 contract A {
+	uint data;
     uint public constant ONE_HOUR = 180;
 	signal priceFeedUpdate(bytes32 data);
+	signal random();
+
     function emitfunc(bytes32 DataSent) public {
 		emitsig priceFeedUpdate(DataSent).delay(ONE_HOUR);
     }
 
+	constructor() public {
+		data = 1;
+	}
 }
+
 contract B {
 	A dut;
 	bytes32 public LocalPriceSum;
     uint public constant ONE_HOUR = 180;
+
 	slot priceReceive(bytes32 obj){
         LocalPriceSum = LocalPriceSum | obj;
     }
+
 	function bindfunc() public view {
 		priceReceive.bind(dut.priceFeedUpdate);
 	}
+
     function detachfunc() public view {
 		priceReceive.detach(dut.priceFeedUpdate);
     }
